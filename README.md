02d6d9b01c3b0bbf6279665c370cfe7681cc84a7
Matt Carter <m@ttcarter.com>
2015-12-19 17:40:16 +1000

    Basic working version

---
+++ README.md
@@ -0,0 +1,17 @@
+coma
+====
+Node CLI to enhance the GNU `sleep` command.
+
+Parses a time expression, waits and quits. Designed to be used within a shell script.
+
+
+	coma next friday && echo "To the pub!"
+
+	coma midnight && echo "It its the witching hour!"
+
+	coma 2h && echo "2 Hours from when we started"
+
+	coma 3pm tomorrow && "Its 3 O'clock!"
+
+
+Can also optionally take a `-c` / `--countdown` parameter to display the time remaining on the console every second.
---
+++ app.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+var colors = require('colors');
+var fromnow = require('fromnow');
+var program = require('commander');
+var moment = require('moment');
+var momentRelative = require('moment-relative');
+var parseMessyTime = require('parse-messy-time');
+
+program
+	.version(require('./package.json').version)
+	.option('-c, --countdown', 'Display a countdown')
+	.option('-v, --verbose', 'Be verbose. Specify multiple times for increasing verbosity', function(i, v) { return v + 1 }, 0)
+	.parse(process.argv);
+
+if (!program.args.length) {
+	console.log('No time specified');
+	process.exit(1);
+}
+
+
+// Try parse with moment
+var time = program.args.join(' ');
+if (program.verbose > 1) console.log(colors.blue('[Coma]'), 'Trying to parse', time);
+
+// Main time parser {{{
+var parsedTime = function(time) {
+	// Parse with moment
+	var parsedMoment = moment(time);
+	if (parsedMoment.isValid()) return parsedMoment.toDate(time);
+
+	// Parse with moment-relative
+	try {
+		var parsedMomentRelative = momentRelative()
+		parsedMomentRelative.relative(time);
+		if (parsedMomentRelative.isValid()) return parsedMomentRelative.toDate(time);
+	} catch (e) {
+		// Ignore moment-relatives weird parsing errors and continue
+	}
+
+	// Parse with parse-messy-time
+	var parsedMessyTime = parseMessyTime(time);
+	if (parsedMessyTime) return parsedMessyTime;
+
+	return false;
+}(time);
+// }}}
+
+// Sanity check that the date is valid {{{
+if (!parsedTime) {
+	console.log(colors.blue('[Coma]'), colors.red('ERROR'), 'Cannot parse date:', time);
+	process.exit(1);
+} else if (parsedTime < (new Date)) {
+	console.log(colors.blue('[Coma]'), colors.red('ERROR'), 'Date', colors.cyan(parsedTime.toString()), 'is in the past');
+	process.exit(1);
+}
+// }}}
+
+if (program.verbose > 1) console.log(colors.blue('[Coma]'), 'Sleeping until', colors.cyan(parsedTime.toString()));
+if (program.verbose) console.log(colors.blue('[Coma]'), 'Sleeping', colors.cyan(fromnow(parsedTime.toString())));
+
+setInterval(function() {
+	var now = (new Date);
+	if (parsedTime < now) {
+		if (program.verbose) console.log(colors.blue('[Coma]'), colors.bold.green('Completed!'));
+		process.exit(0);
+	} else if (program.countdown) {
+		console.log(colors.blue('[Coma]'), 'Remaining time', colors.cyan(fromnow(parsedTime.toString())));
+	}
+}, 1000);
